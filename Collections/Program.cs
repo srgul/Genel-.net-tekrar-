1ef652844547bc7d973ada71fcd2dd5d60842b99
srgul <saitramazangul@gmail.com>
2021-10-19 17:19:56 +0300

    4. ders

---
+++ Collections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "sait", "ramazan", "gül", "yasin", "gül" };
+            Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "sait", "ramazan", "gül", "yasin", "eren" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("sait ramazan");
+            Console.WriteLine(isimler2[5]);
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi ");
+        }
+
+        public void update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Güncellendi ");
+        }
+
+        // eğer bir işlem sonucunu tekrardan kullanmak istemiyorsan void kullanmalısın. 
+        // eğer işlemden geri dönen datayı kullanacaksan ona bir değer atarak func yazmalısın string ise string int ise int 
+
+
+        public int Topla(int Sayi1, int Sayi2)
+        {
+            return Sayi1 + Sayi2;  //işlem sonucunu returnle geri kullanılır*
+        }
+
+        public void Topla2(int Sayi1, int Sayi2)
+        {
+            Console.WriteLine( Sayi1 + Sayi2 ); // işlem sonucunu sadece ekrana bastırır sonucu kullanamazsın*
+        }
+    }
+}
